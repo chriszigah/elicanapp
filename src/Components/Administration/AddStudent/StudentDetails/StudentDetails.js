d7c821a013f26d64804402d54b6c696f6bfcc8c5
Christian Zigah <efochristian@outlook.com>
2019-09-24 18:03:37 +0100

    Finish student details

--- src/Components/Administration/AddStudent/StudentDetails/StudentDetails.js
+++ src/Components/Administration/AddStudent/StudentDetails/StudentDetails.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './StudentDetails.css';
-//import clsx from 'clsx';
-//import { makeStyles } from '@material-ui/core/styles';
-//import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import DateFnsUtils from '@date-io/date-fns';
@@ -37,19 +37,23 @@ import {
          <h1> Enter Student Details </h1>
           <form className="formwrapper" noValidate autoComplete="off">
             <Grid container justify="space-between" alignItems="flex-start" >
+              <p>
               <TextField
                 label="First Name"
                 placeholder="First Name"
                 className='textField'
                 onChange={''}
-                shrink={false}
               />
+              </p>
+              <p>
                <TextField
                 label="Middle Name"
                 placeholder="Middle Name"
                 className='textField'
                 onChange={''}
               />
+              </p>
+              <p>
               <TextField
                 label="Surname"
                 data-shrink={false}
@@ -57,6 +61,16 @@ import {
                 className='textField'
                 onChange={''}
               />
+              </p>
+              <p> 
+                 <InputLabel htmlFor="age-helper">Gender</InputLabel>
+                    <Select>
+                    <MenuItem value="female">Female</MenuItem>
+                    <MenuItem value="male">Male</MenuItem>
+                </Select>
+              </p>
+              <br/>
+              <p>
               <MuiPickersUtilsProvider utils={DateFnsUtils}>
                 <KeyboardDatePicker
                   label="Date Of Birth"
@@ -68,6 +82,47 @@ import {
                   }}
                 />
               </MuiPickersUtilsProvider>
+              </p>
+              <p>
+              <TextField
+                label="Place of Birth"
+                placeholder="Place of Birth"
+                className='textField'
+                onChange={''}
+              />
+              </p>
+              <p>
+              <TextField
+                label="Mother Tongue"
+                placeholder="Mother Tongue"
+                className='textField'
+                onChange={''}
+              />
+              </p>
+              <p>
+              <TextField
+                label="Religion"
+                placeholder="Religion"
+                className='textField'
+                onChange={''}
+              />
+              </p>
+              <p>
+              <TextField
+                label="Religious Denomination"
+                placeholder="Religious Denomination"
+                className='textField'
+                onChange={''}
+              />
+              </p>
+              <p>
+              <TextField
+                label="Language Spoken"
+                placeholder="Language Spoken"
+                className='textField'
+                onChange={''}
+              />
+              </p>
             </Grid>
             </form>
           </div>
